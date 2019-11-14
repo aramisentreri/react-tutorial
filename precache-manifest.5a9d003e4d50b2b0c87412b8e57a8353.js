39702d6a4401d59329a1fcb3a80e6879a60e7756
Gustavo <aramis.entreri@gmail.com>
2019-11-13 17:24:05 -0800

    Updates

---
+++ precache-manifest.5a9d003e4d50b2b0c87412b8e57a8353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cce3836932584962e77c304afd8713",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a9a9c10415acef2a6792",
+    "url": "/react-tutorial/static/css/main.aff784d2.chunk.css"
+  },
+  {
+    "revision": "34e30f6b7bd5ad11cf82",
+    "url": "/react-tutorial/static/js/2.9b6246ed.chunk.js"
+  },
+  {
+    "revision": "a9a9c10415acef2a6792",
+    "url": "/react-tutorial/static/js/main.edd238a5.chunk.js"
+  },
+  {
+    "revision": "23cd54c0b15ae01a9f16",
+    "url": "/react-tutorial/static/js/runtime-main.09266777.js"
+  }
+]);
